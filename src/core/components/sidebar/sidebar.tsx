89af45c5923d0b70cf9f6032acb91f8cbffdeb36
zotovy <the1ime@yandex.ru>
2022-08-14 22:53:59 +0300

    feat(sidebar): add active tabs

--- src/core/components/sidebar/sidebar.tsx
+++ src/core/components/sidebar/sidebar.tsx
@@ -1,48 +1,65 @@
 import React from "react";
 import styles from "./sidebar.module.scss";
-import {SidebarConfig} from "@core/types/layout";
-import {Divider, Spacer, Tooltip, useBoolean, useColorMode} from '@chakra-ui/react'
-import {Link} from "react-router-dom";
-import {ChevronRight} from "react-feather";
+import { NavPage, SidebarConfig } from "@core/types/layout";
+import { Divider, Spacer, Tooltip, useBoolean, useColorMode } from '@chakra-ui/react'
+import { Link } from "react-router-dom";
+import { ChevronRight } from "react-feather";
 import classNames from "classnames";
+import { useActiveTab } from "@core/hooks/use-active-tab";
 
 
 export const Sidebar: React.FC<SidebarConfig> = React.memo((props) => {
     const [open, setOpen] = useBoolean(false)
     const {colorMode} = useColorMode()
 
-    return <div className={classNames(styles.sidebar, {[styles.open]: open})} data-dark={colorMode === "dark"}>
-        <div className={styles.logoItem}>
+    return <div className={ classNames(styles.sidebar, {[styles.open]: open}) } data-dark={ colorMode === "dark" }>
+        <div className={ styles.logoItem }>
             T
             <span>Company</span>
         </div>
-        <div className={styles.items}>
+        <div className={ styles.items }>
             {
                 props.items.map(item => {
                     if (item.type === "divider") return <Divider
                         bg="var(--chakra-colors-chakra-border-color)"
-                        marginY={15}/>
+                        marginY={ 15 }/>
 
-                    return <Tooltip
-                        label={item.text}
-                        placement='right'
-                        bg={colorMode === "light" ? "var(--hint)" : undefined}
-                        color={colorMode === "light" ? "white" : undefined}
-                        isDisabled={open}
-                        borderRadius={5}
-                        boxShadow="none"
-                        openDelay={750}>
-                        <Link to={item.url} className={styles.item} data-dark={colorMode === "dark"}>
-                            <div className={styles.icon}>{item.icon}</div>
-                            <span className={styles.text}>{item.text}</span>
-                        </Link>
-                    </Tooltip>
+                    return <SidebarItem item={item} open={open}/>
                 })
             }
         </div>
         <Spacer/>
-        <div className={styles.bottom} onClick={setOpen.toggle}>
-            <ChevronRight size={28}/>
+        <div className={ styles.bottom } onClick={ setOpen.toggle }>
+            <ChevronRight size={ 28 }/>
         </div>
     </div>
+})
+
+type SidebarItemProps = {
+    item: NavPage,
+    open: boolean,
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = React.memo((props) => {
+    const {colorMode} = useColorMode()
+    const active = useActiveTab(props.item.url)
+    
+    return <Tooltip
+        label={ props.item.text }
+        placement='right'
+        bg={ colorMode === "light" ? "var(--hint)" : undefined }
+        color={ colorMode === "light" ? "white" : undefined }
+        isDisabled={ props.open }
+        borderRadius={ 5 }
+        boxShadow="none"
+        openDelay={ 750 }>
+        <Link
+            to={ props.item.url }
+            className={ styles.item }
+            data-active={ active }
+            data-dark={ colorMode === "dark" }>
+            <div className={ styles.icon }>{ props.item.icon }</div>
+            <span className={ styles.text }>{ props.item.text }</span>
+        </Link>
+    </Tooltip>
 })
